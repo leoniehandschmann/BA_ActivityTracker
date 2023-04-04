033ae5b4908634c6ba83a09b10d36278b0e53001
leoniehandschmann <leonie.handschmann@stud.uni-regensburg.de>
2023-04-04 15:14:43 +0200

    Update ScreenTimeTracking.java

--- app/src/main/java/com/example/activitytracker/ScreenTimeTracking.java
+++ app/src/main/java/com/example/activitytracker/ScreenTimeTracking.java
@@ -8,6 +8,7 @@ import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -23,10 +24,13 @@ import androidx.fragment.app.Fragment;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class ScreenTimeTracking extends Fragment{
 
-    TextView tv_screenTime;
+    private TextView tv_screenTime;
+    private Long usageTimeMillis;
+
     public ScreenTimeTracking(){
 
     }
@@ -38,7 +42,7 @@ public class ScreenTimeTracking extends Fragment{
         tv_screenTime = view.findViewById(R.id.tv_usage);
 
         if(checkUsagePermission(getActivity().getApplicationContext())){
-            showAppUsage(getActivity().getApplicationContext());
+            showAppUsage();
         }else{
             startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
         }
@@ -46,7 +50,6 @@ public class ScreenTimeTracking extends Fragment{
 
         return view;
     }
-
 
     private boolean checkUsagePermission(Context c){
         AppOpsManager appOps = (AppOpsManager)
@@ -57,12 +60,37 @@ public class ScreenTimeTracking extends Fragment{
     }
 
 
-    private void showAppUsage(Context c) {
+    private void showAppUsage() {
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+
+            @Override
+            public void run() {
+                try{
+                    getAppUsage(getActivity().getApplicationContext());
+                    long minutes = TimeUnit.MILLISECONDS.toMinutes(usageTimeMillis);
+                    long seconds = TimeUnit.MILLISECONDS.toSeconds(usageTimeMillis)
+                            - TimeUnit.MINUTES.toSeconds(minutes);
+                    tv_screenTime.setText(String.valueOf(minutes) + " Minuten  " + String.valueOf(seconds) + " Sekunden");
+                }
+                catch (Exception e) {
+                    Log.d("updateTV","not successful");
+                }
+                finally{
+                    handler.postDelayed(this, 30000);
+                }
+            }
+        };
+        handler.postDelayed(runnable, 30000);
+
+    }
+
+    private long getAppUsage(Context c){
         UsageStatsManager usageStatsManager = (UsageStatsManager) c.getSystemService(Context.USAGE_STATS_SERVICE);
         long beginIntervalMillis = System.currentTimeMillis() - 1 * 24 * 60 * 60 * 1000;
         Map<String, UsageStats> usageStatsMap = usageStatsManager.queryAndAggregateUsageStats(beginIntervalMillis,System.currentTimeMillis());
-        long totalTimeUsageInMillis = usageStatsMap.get("com.google.android.calendar").getTotalTimeInForeground();
-        tv_screenTime.setText(String.valueOf(totalTimeUsageInMillis/60000));
+        usageTimeMillis = usageStatsMap.get("com.google.android.calendar").getTotalTimeInForeground();
+        return usageTimeMillis;
     }
 
 
